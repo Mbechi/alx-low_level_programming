8a57f7b3baad657b31348da9d38f7ac69224a446
mbechi <agnessmminaa@gmail.com>
2023-03-23 06:17:22 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,18 +2,17 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: the character to check
- *
+ * @c: char to check
  * Return: 1 if c is uppercase, 0 otherwise
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
-		return 1;
-	}
+		return (1);
+
 	else
-	{
-		return 0;
-	}
+
+		return (0);
+
 }
