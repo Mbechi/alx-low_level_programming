6aced9641f2f8d8311b5ce20954d6350d0d80ab8
mbechi <agnessmminaa@gmail.com>
2023-04-03 11:11:54 -0700

    even_more_pointers_arrays_strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,6 +10,6 @@ void print_chessboard(char (*a)[8]);
 char *_strchr(char *s, char *c);
 char *_strstr(char *haystack, char *needle);
 unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, *accept);
+char *_strpbrk(char *s, char *accept);
 
 #endif
