8dff0e81e42475ea7a24383b22a8faf19fbfbe0d
mbechi <agnessmminaa@gmail.com>
2023-04-03 12:46:28 -0700

    even_more_pointers_arrays_strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 char *_memset(char *s, char b, unsigned int n);
 void set_string(char **s, char *to);
 int _putchar(char c);
-char print_diagsums(int *a, int size);
+void print_diagsums(int *a, int size);
 char *_memcpy(char *dest, char *src, unsigned int n);
 void print_chessboard(char (*a)[8]);
 char *_strchr(char *s, char *c);
