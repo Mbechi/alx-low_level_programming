dc516b5963816209213cf8aef17aebeb31f7f6e1
Mbechi <agnessmminaa@gmail.com>
2023-04-20 12:45:26 -0700

    variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,11 +11,11 @@ void print_all(const char * const format, ...)
 {
 	int i = 0;
 	char *str, *sep = "";
-	
+
 	va_list list;
-	
+
 	va_start(list, format);
-	
+
 	if (format)
 	{
 		while (format[i])
@@ -45,7 +45,7 @@ void print_all(const char * const format, ...)
 			i++;
 		}
 	}
-	
+
 	printf("\n");
 	va_end(list);
 }
