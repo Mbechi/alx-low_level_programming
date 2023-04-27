651e9fdf8ef9c55c4b3218529ae43e1f43c7a5d5
Mbechi <agnessmminaa@gmail.com>
2023-04-27 16:11:28 -0700

    singly_linked_lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void first(void)_attribute;
+
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ */
+
+void firt(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
